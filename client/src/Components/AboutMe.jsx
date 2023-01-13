ebc4f2d79bd0688ae0ca32da4765effeeb54f485
jwludwig7 <jwludwig7@gmail.com>
2023-01-13 12:46:18 -0500

    jan 13 second commit

--- client/src/Components/AboutMe.jsx
+++ client/src/Components/AboutMe.jsx
@@ -10,9 +10,9 @@ const AboutMe = () => {
                 <h1>About Me...</h1>
                 <p>Hello! My name is Jordan and I am a Full Stack Developer. I was working as a sales rep for a dairy company
                     until August 2022. I knew I was meant for something better. Before I was laid off I was trying to find
-                    something new to do with my career. Software Developement always interested me and I would always see the YouTube ads about Software development. Then BOOM... The universe said
-                    "your life is not meant to be staring at milk all day". I trasnitioned into software Developement and found myself at Coding Dojo. There I would be studying several stacks of technology in an intense coding bootcamp. Although it was a lot of work it was easliy the best decision I made by far. Now I am currently looking for a comapny that is willing to give me the opportunity to join their coding team!</p>
-                <p>Besides Software Developement, I have a passion for playing video games, editing gaming clips, snowboarding, and
+                    something new to do with my career. Software Development always interested me and I would always see the YouTube ads about Software Development. Then BOOM... The universe said
+                    "your life is not meant to be staring at milk all day". I transitioned into Software Development and found myself at Coding Dojo. There I would be studying several stacks of technology in an intense coding bootcamp. Although it was a lot of work it was easliy the best decision I made by far. Now I am currently looking for a comapny that is willing to give me the opportunity to join their coding team!</p>
+                <p>Besides Software Development, I have a passion for playing video games, editing gaming clips, snowboarding, and
                     lacrosse. </p>
                 <div className='flex'>
                     <img className='imgSize p-1' src={lax} alt="lax" />
