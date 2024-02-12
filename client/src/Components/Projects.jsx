866dcd78cf54077aa44d81d3abd3dd64219ac507
jwludwig7 <jwludwig7@gmail.com>
2024-02-12 17:26:30 -0500

    2/12 commit

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -7,6 +7,10 @@ import jJokelogo from '../img/jjoke-logo.png'
 import bBoxvid from '../img/bBox_Trim.mp4'
 import bBoxlogo from '../img/bBox.png'
 import githublogo from '../img/github.png'
+import weddingPlannerPic from '../img/weddingPlannerPic.png'
+import weddingPlannerVid from '../img/weddingPlannerVid.mp4'
+import jumpStartPic from '../img/jumpStartPic.png'
+import jumpStartVid from '../img/jumpStartVid.mp4'
 import '../Components/Projects.css'
 
 const Projects = () => {
@@ -86,6 +90,61 @@ const Projects = () => {
                                 loop />
                         </div>
                     </div>
+                    <hr />
+                    <div>
+                        <div className='d-flex justify-content-between mt-3 p-3'>
+                            <h3>Wedding Planner</h3>
+                            <a href="https://github.com/jwludwig7/Coding-Dojo/tree/master/CSharp/ORMs/beltReview/weddingPlanner"><img src={githublogo} alt="github" /></a>
+                        </div>
+                        <div className='d-flex smallerscreen'>
+                            <h6 className='p-3'>
+                                <ul>
+                                    <li className='p-2 psize'>Wedding planner website that has one to many and many to many relationships.</li>
+                                    <li className='p-2 psize'>Has a login and registration for the users, when user logs in they will be greeted with conditional rendering.</li>
+                                    <li className='p-2 psize'>The dashboard will show all the available weddings with a guest count, the date, name of the wedding, and the action you can take, if you created the wedding then you can delete it, if you are not the creator of the wedding then you can rsvp or un-rsvp to the wedding. </li>
+                                    <li className='p-2 psize'>When you click on the wedding name you can be taken to a one-wedding page and it will show a list of guests, the date, and the name of the wedding.</li>
+                                    <li className='p-2 psize'>Nav bar for easy navigation. User can create weddings with form with proper validation. MVC type of C# project. Db used is mysql.</li>
+                                    <li className='p-2 psize'>Functionality more then looks.</li>
+                                </ul>   
+                            </h6>
+                            <hr />
+                            <HoverVideoPlayer
+                                className='zoom vidwidth p-2'
+                                videoSrc={weddingPlannerVid}
+                                pausedOverlay={
+                                    <img className='vidimg' src={weddingPlannerPic} alt='weddingPlannerPic' />
+                                }
+                                loop />
+                        </div>
+                    </div>
+                    <hr />
+                    <div>
+                        <div className='d-flex justify-content-between mt-3 p-3'>
+                            <h3>Jump Start: Support a Project</h3>
+                            <a href="https://github.com/jwludwig7/Coding-Dojo/tree/master/CSharp/beltExam/beltExamCSharp"><img src={githublogo} alt="github" /></a>
+                        </div>
+                        <div className='d-flex smallerscreen'>
+                            <h6 className='p-3'>
+                                <ul>
+                                <li className='p-2 psize'>Disclamer, this is not for looks more for fucntionality. This was my exam and we were given a wire frame and were expected to have it completed and fully functional in 5 hours.</li>
+                                    <li className='p-2 psize'>It is a kickstarter inspired project, made with C#, ASP.NET6.</li>
+                                    <li className='p-2 psize'>Front page is greeted with a login and reg with authorization, Database used to store all the info is MySQL.</li>
+                                    <li className='p-2 psize'>The dashboard page is a table list of all the available projects that other users and logged user have made. </li>
+                                    <li className='p-2 psize'>Table shows the project name, total funded, creator, and the actions you can take for the project, if you created it you can delete it and view it, if you did not create it you can just view the project.</li>
+                                    <li className='p-2 psize'>Users can create the project and the form asks for the title, goal, end date of the project funding, and a description of the project, form has proper validation. When viewing the one-project it shows the goal, the amount of current support in USD, the number of supported users, the end date, and if it's the current users project, conditional rendering will come in and not allow user to pledge to their own project. If it's not the users project they can add an amount of support in USD. MVC type of C# project.</li>
+                                </ul>   
+                            </h6>
+                            <hr />
+                            <HoverVideoPlayer
+                                className='zoom vidwidth p-2'
+                                videoSrc={jumpStartVid}
+                                pausedOverlay={
+                                    <img className='vidimg' src={jumpStartPic} alt='jumpStartPic' />
+                                }
+                                loop />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
